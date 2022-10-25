bf269cb0cbdb09f53d8bd982c34e6f4fd5692a6b
almog ram <almoghr36@gmail.com>
2022-10-25 17:59:21 +0300

    first lesson 23.10.22 - js fundamentals

---
+++ javascript - first lesson/main.js
@@ -0,0 +1,69 @@
+// const age = 31;
+
+// console.log('age');
+
+// const name = 'almog';
+
+// let name3 = `shalom`;
+
+// let name2;
+// name2 = "dror";
+
+
+// name3 = 550;
+
+// const num = 50;
+// const num2 = num+12;
+
+// console.log(num, num2);
+
+// let isAlmogHappy = true;
+
+// 'dsadas' (string), 30 (number), true\false (boolean), [], {}, function, date,
+
+
+let age = 30
+console.log(age)
+age = age + 1 // 31
+console.log(age)
+age = age - 5 // 26
+console.log(age)
+age = age * 2
+console.log(age)
+age = age / 26
+console.log(age)
+
+age += 5 // מגדיל בכמה שנבקש ממנו
+age++ // תמיד מגדיל ב 1
+age -= 2
+age--
+
+const something = 5 + 'hello'
+let plus = 1 + '1' // 1+1=2 '1'+'1' = '11'
+// console.log(parseInt(plus))
+plus += 5
+console.log(plus)
+console.log(something)
+const myMoney = 200 + 1500;
+const yerusha = 10000000
+console.log(1700)
+
+const total = myMoney + (yerusha - yerusha * 0.35)
+console.log(total)
+
+let x;
+console.log(x)
+console.log(age)
+
+console.log('i am here!!!!!!!')
+
+const myFriend = 'EDEN';
+const myFriend2 = 'eden'
+console.log(myFriend === myFriend2)
+const numb = 5
+const numb2 = '5'
+console.log(numb === numb2)
+console.log(numb == numb2) // לא להשתמש בשני שווה אף פעם!!!!!!
+
+const myHobby = prompt('הכנס את התחביב האהוב עליך')
+console.log(myHobby)
