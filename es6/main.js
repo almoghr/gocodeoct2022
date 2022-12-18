fdd8beab87c5050b2ff5167982544e10d67b831b
almoghr <almoghr36@gmail.com>
2022-12-18 21:16:02 +0200

    all es6 materials

---
+++ es6/main.js
@@ -0,0 +1,81 @@
+
+// // console.log(hello)
+
+// // var hello = 'hello there i am almog'
+
+// function hello(){
+//     return 'hello world'
+// }
+// const hello4 = () => 'hello world'
+
+// const obj = {first:'almog', last:'ram'}
+// const obj2 = {first:'almog', last:'ram'}
+// const obj3 = obj
+// const hello2 = 'hello there i am almog'
+
+// let hello3 = 'hello there i am almog'
+
+// console.log(hello + ' ' + hello2 + ' ' + hello3 + ' ' + 'i want to write something else')
+// console.log(`${hello} ${hello2} ${hello3} i want to write something else and also ${obj.first + ' ' + obj.last} ${obj.last}`)
+
+// console.log(`this is my message for you "${hello()}"`)
+
+// const numArr = [5,45,78,54,1,4,8,68,9,4,178,17,810,9,48,94,1,9841,5]
+
+// const onlyBiggerThan50 = () => numArr.filter((number) => number > 50);
+
+// console.log(onlyBiggerThan50())
+
+// function onlyBiggerThan50NumberTwo(){
+//     return numArr.filter(function(number){
+//         return number > 50
+//     })
+// }
+
+// console.log(hello4())
+
+// console.log(obj === obj2)
+
+// console.log(obj3 === obj)
+
+// obj.first = 'israel'
+
+// console.log(obj, obj3)
+
+// const obj4 = {...obj}
+
+// console.log(obj4)
+// obj4.last = 'damri'
+
+// console.log(obj, obj3, obj4)
+
+// const numArr2 = [...numArr]
+
+// numArr2.pop()
+
+// console.log(numArr, numArr2)
+
+// const object1 = {
+//     a: 'somestring',
+//     b: 42,
+//     c: false,
+//     d:21,
+//     e:{one:1,two:2,three:3},
+//     f:54378,
+//     g:true,
+//     myNickName: 'boss'
+//   };
+  
+  
+//   const someFunc = ({f,e,a, myNickName,  ...rest}) => {
+//     const {one,two,three} = e
+//     console.log(rest)
+//     console.log(a,e,f)
+//     console.log(one,two,three, myNickName)
+//     //logic
+        
+//     return true
+//   } 
+
+//   someFunc(object1)
+
---
+++ modal-exmaple/main.js
@@ -0,0 +1,25 @@
+// const modal = document.querySelector('.modal')
+// const modalMaker = document.querySelector('.modalMaker')
+// const closeModal = document.querySelector('.closeModal')
+
+// async function getDetailsForModal(){
+//     // const response = await fetch('someurl')
+//     // const details = await response.json();
+//     // details.forEach(function(item){
+//     //     //
+//     //     //
+//     //     // a lot of logic
+//     //     //
+//     //     //
+
+//     // })
+//     modalMaker.addEventListener('click', function(){
+//         modal.classList.toggle('hidden')
+//     })
+//     closeModal.addEventListener('click', function(){
+//         modal.classList.toggle('hidden')
+//     })
+// }
+
+// getDetailsForModal()
+
