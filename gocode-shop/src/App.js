d4f1d3413d0e23b28502aac7eff0fe1a78c63b14
almoghr <almoghr36@gmail.com>
2022-12-25 20:32:24 +0200

    CRA + props didnt do useState

---
+++ gocode-shop/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './components/Button/Button'
+import {arr, arr2} from './data/data'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ gocode-shop/src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Button.css'
+
+
+const Button = () => {
+
+    // js logic
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
