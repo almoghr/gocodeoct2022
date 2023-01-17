45876cd6962a311cb2530fd66de3b1c9ed045021
almoghr <almoghr36@gmail.com>
2023-01-17 19:46:11 +0200

    basics of nodejs + express+ routing

---
+++ node/index.js
@@ -0,0 +1,10 @@
+// console.log('hello world')
+// console.log('hi')
+// console.log(5+5+5)
+
+// console.log(process.argv)
+
+// if(process.argv[2] === "test"){
+//     console.log("welcome superuser")
+// }
+
---
+++ node/server.js
@@ -0,0 +1,23 @@
+//imports
+import express from 'express'
+import cors from 'cors'
+
+//the initialising of the server itself
+const app = express()
+
+// middlewares for the server
+app.use(express.json())
+app.use(cors())
+
+//routes
+//build from path and a function that called controller
+app.get('/calculator', async (req,res) => {
+    res.send({message:"the answer of is"})
+})
+
+//listener at the bottom which concludes the 
+//listening function to fulfuill all of the requests
+app.listen(8000,() => {
+    console.log('i am listening on port 8000')
+})
+
